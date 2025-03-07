1dfc4030ad578039f893b4e06ab2aa922f26c0d4
unknown <saadsyed950@gmail.com>
2025-03-07 23:26:38 +0530

    User logout and check profile

--- src/api/routes/user.route.ts
+++ src/api/routes/user.route.ts
@@ -1,14 +1,24 @@
 // src\api\routes\user.route.ts
 
+import { authMiddleware } from "../../middleware/auth.middleware";
 import {
+  checkUserProfileController,
   deleteUserController,
+  loginUserController,
+  logoutUserController,
   registerUserController,
 } from "../controllers/user.controller";
 import { Router } from "express";
 
 const userRoutes = Router();
 
-userRoutes.post("/", registerUserController);
-userRoutes.post("/delete/:user_id", deleteUserController);
+// Auth routes
+userRoutes.post("/register", registerUserController);
+userRoutes.post("/login", loginUserController);
+userRoutes.post("/logout", logoutUserController);
+userRoutes.get("/profile", authMiddleware, checkUserProfileController);
+
+// Delete route
+userRoutes.delete("/delete/:user_id", deleteUserController);
 
 export default userRoutes;
