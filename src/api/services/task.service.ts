6297fcc0de0ab64b969c4265757f228c4fe7c278
unknown <saadsyed950@gmail.com>
2025-03-08 23:31:13 +0530

    All services of task

--- src/api/services/task.service.ts
+++ src/api/services/task.service.ts
@@ -3,15 +3,60 @@
 import { prisma } from "../../lib/prisma";
 
 // Service to create a task
-export const createTaskService = async (title: string, description: string) => {
+export const createTaskService = async (
+  title: string,
+  description: string,
+  user_id: string
+) => {
   // Check if task already exists
-  const existingTask = await prisma.task.findUnique({ where: { title } });
+  const existingTask = await prisma.task.findUnique({
+    where: { title, user_id },
+  });
   if (existingTask) throw new Error("Task already exists");
 
   return await prisma.task.create({
     data: {
       title,
       description,
+      user_id,
     },
+  });
+};
+
+// Service to get all task for a user
+export const getAllTaskService = async (user_id: string) => {
+  return await prisma.task.findMany({
+    where: { user_id },
+  });
+};
+
+// Service to update task
+export const updateTaskService = async (
+  task_id: string,
+  title: string,
+  description: string,
+  user_id: string
+) => {
+  // Get the assigned task
+  const task = await prisma.task.findUnique({ where: { task_id, user_id } });
+  if (!task) throw new Error("Task is not assigned to the user"); // Throw if task is not found
+
+  return await prisma.task.update({
+    where: { task_id },
+    data: {
+      title,
+      description,
+    },
+  });
+};
+
+// Service to delete task
+export const deleteTaskService = async (task_id: string, user_id: string) => {
+  // Get the assigned task
+  const task = await prisma.task.findUnique({ where: { task_id, user_id } });
+  if (!task) throw new Error("Task is not assigned to the user"); // Throw if task is not found
+
+  return await prisma.task.delete({
+    where: { task_id, user_id },
   });
 };
